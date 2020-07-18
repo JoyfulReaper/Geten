9dd0e8d719c8cb9b39dea5dc34a06671ef42bedf
Kyle Givler <xvegan88x@gmail.com>
2020-07-18 10:11:11 -0400

    Started Look Command

---
+++ TextEngine/Commands/LookCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TextEngine.CommandParsing;
+using TextEngine.Parsing;
+
+namespace TextEngine.Commands
+{
+    [CommandName("look")]
+    class LookCommand : ITextCommand
+    {
+        private Token<CommandKind> LookAt { get; set; }
+        public void Invoke()
+        {
+            if (LookAt == null)
+                TextEngine.AddMessage(TextEngine.Player.Location.LookDescription);
+        }
+
+        public ITextCommand Parse(CommandParser parser)
+        {
+            try
+            {
+                LookAt = parser.MatchToken(CommandKind.Identifier);
+            } catch (Exception e)
+            {
+                LookAt = null;
+            }
+
+            return new LookCommand();
+        }
+    }
+}
