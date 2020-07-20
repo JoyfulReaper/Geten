756183af114ed174cb388eb2057b17ac12def46e
Chris Anders <filmee24@gmail.com>
2020-07-20 15:50:52 +0200

    Add SymbolTable

---
+++ TextEngine/Parsing/SymbolTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextEngine.Parsing
+{
+    public static class SymbolTable
+    {
+        private static Dictionary<string, object> _objects = new Dictionary<string, object>();
+
+        public static void Add(string name, object instance)
+        {
+            if (_objects.ContainsKey(name)) throw new Exception($"'{name}' is already declared");
+
+            _objects.Add(name, instance);
+        }
+
+        public static T GetInstance<T>(string name)
+        {
+            if (!_objects.ContainsKey(name)) throw new Exception($"'{name}' is not declared");
+
+            return (T)_objects[name];
+        }
+    }
+}
