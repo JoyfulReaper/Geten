913d4273da5b1547ae9d1eca3d7254e583df16d1
Chris Anders <filmee24@gmail.com>
2020-07-20 16:34:40 +0200

    Add EventManager

---
+++ TextEngine/EventManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Concurrent;
+using TextEngine.Parsing.Syntax;
+
+namespace TextEngine
+{
+    public static class EventManager
+    {
+        private static ConcurrentDictionary<string, ConcurrentQueue<BlockNode>> _subscriptions = new ConcurrentDictionary<string, ConcurrentQueue<BlockNode>>();
+
+        public static void Subscribe(string name, BlockNode body)
+        {
+            if(_subscriptions.ContainsKey(name))
+            {
+                _subscriptions[name].Enqueue(body);
+            }
+            else
+            {
+                var bodyList = new ConcurrentQueue<BlockNode>();
+                bodyList.Enqueue(body);
+                _subscriptions.TryAdd(name, bodyList);
+            }
+        }
+
+        public static void Raise(string name)
+        {
+            var bodies = _subscriptions[name];
+            foreach (var b in bodies)
+            {
+                //b.Accept()// need a way to run the body
+            }
+        }
+    }
+}
