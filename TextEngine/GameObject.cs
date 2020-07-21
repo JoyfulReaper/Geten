7ed8f8ac5527c9ecc1e5ba411d5feed6af973b61
Chris Anders <filmee24@gmail.com>
2020-07-21 12:21:13 +0200

    Add GameObject
    need to be inherited

---
+++ TextEngine/GameObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using TextEngine.Parsing.Syntax;
+
+namespace TextEngine
+{
+    public abstract class GameObject
+    {
+        //property bag for changable properties by script
+        private Dictionary<string, object> _properties = new Dictionary<string, object>();
+
+        public void SetProperty(string name, object value)
+        {
+            if(_properties.ContainsKey(name))
+            {
+                _properties[name] = value;
+            }
+            else
+            {
+                _properties.Add(name, value);
+            }
+        }
+
+        public T GetProperty<T>(string name)
+        {
+            return (T)GetProperty(name);
+        }
+
+        public object GetProperty(string name)
+        {
+            if(_properties.ContainsKey(name))
+            {
+                return _properties[name];
+            }
+
+            return null;
+        }
+
+
+        public string Name { get; set; }
+
+        public void MatchPropertyList(PropertyList list)
+        {
+            foreach (var p in list)
+            {
+                SetProperty(p.Key.Text, list[p.Key.Text]);
+            }
+        }
+
+    }
+}
