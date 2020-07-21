b9441071df44e80ffca031c180f76efdcdedc62c
Chris Anders <filmee24@gmail.com>
2020-07-21 12:24:34 +0200

    Start Implementing CaseInsensitive String

---
+++ TextEngine/CaseInsensitiveString.cs
@@ -0,0 +1,22 @@
+﻿namespace TextEngine
+{
+    public class CaseInsensitiveString
+    {
+        private readonly string src;
+
+        public CaseInsensitiveString(string src)
+        {
+            this.src = src.ToLower();
+        }
+
+        public static implicit operator string(CaseInsensitiveString str)
+        {
+            return str.src;
+        }
+
+        public static implicit operator CaseInsensitiveString(string str)
+        {
+            return new CaseInsensitiveString(str);
+        }
+    }
+}
