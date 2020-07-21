835d061c86de27597c201a04b2dd143f1fa8814e
Chris Anders <filmee24@gmail.com>
2020-07-21 15:28:49 +0200

    Add operator overload for CaseSentisitveString

--- TextEngine/CaseInsensitiveString.cs
+++ TextEngine/CaseInsensitiveString.cs
@@ -18,5 +18,37 @@
         {
             return new CaseInsensitiveString(str);
         }
+
+        public static bool operator ==(CaseInsensitiveString src, string check)
+        {
+            return src.src == check;
+        }
+
+        public static bool operator !=(CaseInsensitiveString src, string check)
+        {
+            return src.src != check;
+        }
+
+        public override string ToString()
+        {
+            return src;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is CaseInsensitiveString other))
+            {
+                return false;
+            }
+
+            throw new System.NotImplementedException();
+        }
+
+        public override int GetHashCode()
+        {
+            return src.GetHashCode();
+        }
+
+        //ToDo: replace all needed strings
     }
 }
