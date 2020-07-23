7bd9cfe51cabe94e4ef229fd88dc382f2aaabd40
Kyle Givler <xvegan88x@gmail.com>
2020-07-22 20:22:08 -0400

    Now actually playable!

--- TextEngine/Program.cs
+++ TextEngine/Program.cs
@@ -25,94 +25,26 @@ SOFTWARE.
 using Geten.Core;
 using Geten.GameObjects;
 using Geten.MapItems;
+using Geten.Parsers.Script;
 using System;
+using System.IO;
 
 namespace Geten
 {
     internal static class Program
     {
-        public static void TestingSetup()
-        {
-        }
-
         private static void Main(string[] args)
         {
-            Player player = new Player("Kyle");
-            TextEngine.Player = player;
-            player.Inventory.Capacity = 100;
+            Directory.SetCurrentDirectory(@"..\..\..\SampleGame");
+            Console.WriteLine(Directory.GetCurrentDirectory());
+            ShowIntro();
 
-            Room r1 = new Room("Starting Room", "Starting Room", "You find yourself in a rather large and dark room", "You look around, It looks like there is an exit to the East!");
-            Room r2 = new Room("Another Room", "Second Room", "You smell a foul scent", "I think something might have died in here!");
-
-            r2.SetSide(Direction.Up, new Roof());
-            r2.SetSide(Direction.Down, new Floor());
-            r2.SetSide(Direction.North, new Wall());
-            r2.SetSide(Direction.East, new Wall());
-            r2.SetSide(Direction.South, new Wall());
-            r2.SetSide(Direction.West, new Exit(r1));
-
-            r1.SetSide(Direction.Up, new Roof());
-            r1.SetSide(Direction.Down, new Floor());
-            r1.SetSide(Direction.North, new Wall());
-            r1.SetSide(Direction.East, new Exit(r2));
-            r1.SetSide(Direction.South, new Wall());
-            r1.SetSide(Direction.West, new Wall());
-
-            TextEngine.AddRoom(r1);
-            Console.WriteLine("Added: {0}", r1.Name);
-
-            if (TextEngine.RoomExists(r2))
-                Console.WriteLine("r2 is there!");
-            else
-                Console.WriteLine("r2 is missing!");
-
-            TextEngine.AddRoom(r2);
-            Console.WriteLine("Added: {0}", r2.Name);
-
-            if (TextEngine.RoomExists(r2))
-                Console.WriteLine("r2 is there!");
-
-            Console.WriteLine("----------------------------------------------------");
-
-            Console.WriteLine(r1.GetSide(Direction.Up).ToString());
-            Console.WriteLine(r1.GetSide(Direction.Down).ToString());
-            Console.WriteLine(r2.GetSide(Direction.West).ToString());
-            Console.WriteLine(r2.GetSide(Direction.East).ToString());
-            Console.WriteLine(r2);
-
-            Console.WriteLine("----------------------------------------------------");
-
-            if (!TextEngine.Player.Inventory.HasItem("Phone"))
-                Console.WriteLine("Player has no phone!");
-
-            Item i1 = new Item("Phone", "It's an Android", true, true);
-
-            player.Inventory.AddItem(i1);
-            Console.WriteLine("Added {0} to player", i1.Name);
-
-            if (TextEngine.Player.Inventory.HasItem("Phone"))
-                Console.WriteLine("You have {0} phone.", player.Inventory.ItemQuantity("Phone"));
-
-            r1.Inventory.AddItem(i1);
-            Console.WriteLine("Added {0} to room", i1.Name);
-
-            if (TextEngine.Player.Inventory.HasItem("Phone"))
-                Console.WriteLine("You have {0} phone.", player.Inventory.ItemQuantity("Phone"));
-
-            player.Inventory.AddItem(i1, 1);
-            Console.WriteLine("Added {0}", i1.Name);
-
-            TextEngine.Player.Inventory.AddItem(i1);
-
-            if (TextEngine.Player.Inventory.HasItem("Phone"))
-                Console.WriteLine("You have {0} phone.", player.Inventory.ItemQuantity("Phone"));
-
-            Console.WriteLine(i1.ToString());
-
-            Console.WriteLine("----------------------------------------------------");
+            string script = System.IO.File.ReadAllText(@"Demo.script");
+            ScriptParser scriptParser = new ScriptParser();
+            var result = scriptParser.Parse(script);
+            result.Accept(new EvaluationVisitor(scriptParser.Diagnostics));
 
             GreedyWrap wrapper = new GreedyWrap(Console.WindowWidth);
-            TextEngine.StartRoom = r1;
             TextEngine.StartGame();
             while (!TextEngine.GameOver)
             {
@@ -122,10 +54,17 @@ namespace Geten
                     Console.WriteLine(wrapper.LineWrap((TextEngine.GetMessage())));
                 }
 
-                Console.Write("Enter command: ");
+                Console.Write("\nEnter command: ");
                 string input = Console.ReadLine();
                 TextEngine.ProccessCommand(input);
             }
+        }
+
+        private static void ShowIntro()
+        {
+            Console.WriteLine("----------------------------------------------------");
+            Console.WriteLine("\nGeten - General Text Adventure Engine\n");
+            Console.WriteLine("----------------------------------------------------\n\n");
         }
     }
 }
