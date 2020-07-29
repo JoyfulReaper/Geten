d4a894ad57b17e189a8a177371fa114fcb9ec006
Chris Anders <filmee24@gmail.com>
2020-07-29 11:29:48 +0200

    Cleanup

--- Geten.Runtime/IO/GameBinaryBuilder.cs
+++ Geten.Runtime/IO/GameBinaryBuilder.cs
@@ -2,7 +2,7 @@
 
 namespace Geten.Runtime.IO
 {
-    public class GameBinaryBuilder
+    public class GameBinaryBuilder : IGameBinaryBuilder
     {
         private BinaryGameDefinitionFile _file = new BinaryGameDefinitionFile();
 
@@ -11,22 +11,9 @@ namespace Geten.Runtime.IO
             return new GameBinaryBuilder();
         }
 
-        public static implicit operator BinaryGameDefinitionFile(GameBinaryBuilder builder)
+        public BinaryGameDefinitionFile GetFile()
         {
-            return builder._file;
-        }
-
-        public GameBinaryBuilder AddSection(CaseInsensitiveString name, byte[] body)
-        {
-            var s = new BinaryGameSection();
-            s.Header.Name = name;
-            s.Header.SectionLength = body.Length;
-            s.Body = body;
-
-            _file.Header.SectionCount++;
-            _file.Sections.Add(s);
-
-            return this;
+            return _file;
         }
     }
 }
