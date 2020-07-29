b2fb259b2d0ee6a10606f92aaa4cd56269709804
Chris Anders <filmee24@gmail.com>
2020-07-29 19:27:13 +0200

    Cleanup

--- Geten.Runtime/Tables/MetadataTable.cs
+++ Geten.Runtime/Tables/MetadataTable.cs
@@ -3,20 +3,20 @@ using System.IO;
 
 namespace Geten.Runtime.Tables
 {
-    public class MetadataTable : BinaryTable<CaseInsensitiveString, CaseInsensitiveString>
-    {
-        public override CaseInsensitiveString ReadKey(BinaryReader br) => br.ReadString();
+	public class MetadataTable : BinaryTable<CaseInsensitiveString, CaseInsensitiveString>
+	{
+		public override CaseInsensitiveString ReadKey(BinaryReader br) => br.ReadString();
 
-        public override CaseInsensitiveString ReadValue(BinaryReader br) => br.ReadString();
+		public override CaseInsensitiveString ReadValue(BinaryReader br) => br.ReadString();
 
-        public override void WriteKey(BinaryWriter bw, CaseInsensitiveString key)
-        {
-            bw.Write(key);
-        }
+		public override void WriteKey(BinaryWriter bw, CaseInsensitiveString key)
+		{
+			bw.Write(key);
+		}
 
-        public override void WriteValue(BinaryWriter bw, CaseInsensitiveString value)
-        {
-            bw.Write(value);
-        }
-    }
+		public override void WriteValue(BinaryWriter bw, CaseInsensitiveString value)
+		{
+			bw.Write(value);
+		}
+	}
 }
