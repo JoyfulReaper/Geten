5ab1e6402a4e92f3c56caa815a03ac167d950bec
Chris Anders <filmee24@gmail.com>
2020-07-29 11:30:06 +0200

    Add TableExtensions

---
+++ Geten.Runtime/TableExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Geten.Core;
+using Geten.Runtime.IO;
+
+namespace Geten.Runtime
+{
+    public static class TableExtensions
+    {
+        public static IGameBinaryBuilder AddTableSection<T>(this IGameBinaryBuilder builder, CaseInsensitiveString name, T table)
+            where T : IBinaryTable, new()
+        {
+            builder.AddSection(name, table.Save());
+
+            return builder;
+        }
+
+        public static T GetTable<T>(this BinaryGameSection s)
+                    where T : IBinaryTable, new()
+        {
+            var instance = new T();
+            instance.Load(s.Body);
+
+            return instance;
+        }
+
+        public static T GetTable<T>(this BinaryGameDefinitionFile f, CaseInsensitiveString name)
+           where T : IBinaryTable, new()
+        {
+            return f[name].GetTable<T>();
+        }
+    }
+}
