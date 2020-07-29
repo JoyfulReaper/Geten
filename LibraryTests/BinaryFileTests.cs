e93b09d9a2c1bb737d5ed08bb77eb7657a53304f
Chris Anders <filmee24@gmail.com>
2020-07-29 20:41:53 +0200

    Cleanup

--- LibraryTests/BinaryFileTests.cs
+++ LibraryTests/BinaryFileTests.cs
@@ -7,43 +7,43 @@ using System.IO;
 
 namespace LibraryTests
 {
-    [TestClass]
-    public class BinaryFileTests
-    {
-        private BinaryGameDefinitionFile bf;
+	[TestClass]
+	public class BinaryFileTests
+	{
+		private BinaryGameDefinitionFile _bf;
 
-        [TestInitialize]
-        public void Init()
-        {
-            var metadata = new MetadataTable
-            {
-                { "name", "Fork Zork" },
-                { "version", "1.0.0.0" }
-            };
+		[TestInitialize]
+		public void Init()
+		{
+			var metadata = new MetadataTable
+			{
+				{ "name", "Fork Zork" },
+				{ "version", "1.0.0.0" }
+			};
 
-            bf = GameBinaryBuilder.Build()
-                .AddSection("Awnser", BitConverter.GetBytes(42))
-                .AddTableSection("MetadataTable", metadata)
-                .GetFile();
-        }
+			_bf = GameBinaryBuilder.Build()
+				.AddSection("Awnser", BitConverter.GetBytes(42))
+				.AddTableSection("MetadataTable", metadata)
+				.GetFile();
+		}
 
-        [TestMethod]
-        public void ReadBinaryFile_Should_Pass()
-        {
-            var ms = new MemoryStream();
-            bf.Save(ms);
+		[TestMethod]
+		public void ReadBinaryFile_Should_Pass()
+		{
+			var ms = new MemoryStream();
+			_bf.Save(ms);
 
-            var tmp = BinaryGameDefinitionFile.Load(new MemoryStream(ms.ToArray()));
-            var metadata = tmp.GetTable<MetadataTable>("MetadataTable");
-        }
+			var tmp = BinaryGameDefinitionFile.Load(new MemoryStream(ms.ToArray()));
+			var metadata = tmp.GetTable<MetadataTable>("MetadataTable");
+		}
 
-        [TestMethod]
-        public void SaveBinaryFile_Should_Pass()
-        {
-            var ms = new MemoryStream();
-            bf.Save(ms);
+		[TestMethod]
+		public void SaveBinaryFile_Should_Pass()
+		{
+			var ms = new MemoryStream();
+			_bf.Save(ms);
 
-            var raw = ms.ToArray();
-        }
-    }
+			var raw = ms.ToArray();
+		}
+	}
 }
