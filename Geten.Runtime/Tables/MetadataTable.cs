35103159ebbcdb7f8a4facb4fdea5ecf9d66989b
Chris Anders <filmee24@gmail.com>
2020-07-29 11:30:23 +0200

    Add MetadataTable

---
+++ Geten.Runtime/Tables/MetadataTable.cs
@@ -0,0 +1,22 @@
+﻿using Geten.Core;
+using System.IO;
+
+namespace Geten.Runtime.Tables
+{
+    public class MetadataTable : BinaryTable<CaseInsensitiveString, CaseInsensitiveString>
+    {
+        public override CaseInsensitiveString ReadKey(BinaryReader br) => br.ReadString();
+
+        public override CaseInsensitiveString ReadValue(BinaryReader br) => br.ReadString();
+
+        public override void WriteKey(BinaryWriter bw, CaseInsensitiveString key)
+        {
+            bw.Write(key);
+        }
+
+        public override void WriteValue(BinaryWriter bw, CaseInsensitiveString value)
+        {
+            bw.Write(value);
+        }
+    }
+}
