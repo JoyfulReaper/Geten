60fad523126b6442fb2b406b85225bbf0b211208
Chris Anders <filmee24@gmail.com>
2020-07-30 16:30:00 +0200

    Start Implementing Compiler

---
+++ GetenCompiler/CompilationVisitor.cs
@@ -0,0 +1,137 @@
+﻿using Geten.Core.Parsers.Script;
+using Geten.Core.Parsers.Script.Syntax;
+using Geten.Core.Parsing.Diagnostics;
+using Geten.Runtime.IO;
+
+namespace GetenCompiler
+{
+	internal class CompilationVisitor : IScriptVisitor
+	{
+		private readonly GameBinaryBuilder _binary;
+		private readonly DiagnosticBag diagnostics;
+
+		public CompilationVisitor(DiagnosticBag diagnostics)
+		{
+			this.diagnostics = diagnostics;
+			_binary = GameBinaryBuilder.Build();
+		}
+
+		public void Visit(BlockNode block)
+		{
+			foreach (var n in block)
+			{
+				n.Accept(this);
+			}
+		}
+
+		public void Visit(AddItemNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(CharacterDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(AskForInputNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(CommandNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(RoutineDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(DecreaseNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(IncreaseNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(DialogCallNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(EventSubscriptionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(ItemDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(KeyDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(LiteralNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(MemorySlotDefinition node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(PlayNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(RemoveItemNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(RoomDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(SetPropertyNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(TellNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(WeaponDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(ExitDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(RecipeBookDefinition node)
+		{
+			throw new System.NotImplementedException();
+		}
+
+		public void Visit(RecipeDefinitionNode node)
+		{
+			throw new System.NotImplementedException();
+		}
+	}
+}
--- GetenCompiler/Program.cs
+++ GetenCompiler/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using Geten.Core.Parsers.Script;
+using Geten.Runtime;
 
 namespace GetenCompiler
 {
 	internal static class Program
 	{
-		private static void Main()
+		private static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			if (args.Length > 0)
+			{
+				var filename = args.First();
+
+				var p = new ScriptParser();
+				var result = p.Parse(File.ReadAllText(filename));
+
+				if (p.Diagnostics.Any())
+				{
+					foreach (var d in p.Diagnostics)
+					{
+						Console.WriteLine(d);
+					}
+					return;
+				}
+				else
+				{
+					result.Accept(new CompilationVisitor(p.Diagnostics));
+				}
+			}
+			else
+			{
+				Console.WriteLine("No Script given to compile");
+			}
 		}
 	}
 }
