6ca5da29a534fd5a44d4cded7f5fdd3e492567b4
Chris Anders <filmee24@gmail.com>
2020-08-01 12:28:42 +0200

    Add Core for GameRepository handling

---
+++ Geten.Core/GameRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Geten.Core
+{
+	public static class GameRepository
+	{
+		public static IEnumerable<string> GetInstalledGames()
+		{
+		}
+
+		public static void InstallGame(string name)
+		{
+		}
+	}
+}
